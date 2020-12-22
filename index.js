a0c5308794ce27389484f513f39747f1b864870b
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 15:14:49 +0530

    adding ScaleUtil class

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const delay = 20 
 const scGap = 0.02 
+
+class ScaleUtil {
+
+    static maxScale(scale, i, n) {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale, i, n) {
+        return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
+    }
+
+    static sinify(scale) {
+        return Math.sin(scale * Math.PI)
+    }
+}
 class Animator {
 
     start(cb) {
