206db30c4c75328372c7712e5c2b58bcd2e8aea1
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 15:10:40 +0530

    adding State class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const delay = 20 
+const scGap = 0.02 
 class Animator {
 
     start(cb) {
@@ -12,6 +13,21 @@ class Animator {
         if (this.animated) {
             this.animated = false 
             clearInterval(this.interval)
+        }
+    }
+}
+
+class State {
+
+    constructor() {
+        this.scale = 0 
+    }
+
+    update(cb) {
+        this.scale += scGap 
+        if (this.scale > 1) {
+            this.scale = 0 
+            cb()
         }
     }
 }
