814f8a39def7be90cd72d554b7048126c8132783
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 17:12:00 +0530

    adding html file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const delay = 20 
+const delay = 30 
 const scGap = 0.02 
 const w = window.innerWidth 
 const h = window.innerHeight
@@ -10,7 +10,7 @@ class ScaleUtil {
     }
 
     static divideScale(scale, i, n) {
-        return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
+        return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
     }
 
     static sinify(scale) {
@@ -50,34 +50,43 @@ class State {
 }
 const animator = new Animator()
 
-Vue.component('sinified-up-box', {
-    template: '<div><button  @click="start">start</button><div :style = "barstyle"></div>',
+const component1 = Vue.component('sinified-up-box', {
+    template: '<div><button  @click="start">start</button><div :style = "barStyle"></div></div>',
     data() {
         return {
-            state: new State()
+            state: new State(), 
+            sf : 0
         }
     }, 
     methods: {
         start() {
             animator.start(() => {
+                this.sf = ScaleUtil.sinify(this.state.scale)
                 this.state.update(() => {
                     animator.stop()
+                    this.sf = 0
                 })
             })
         }
     },
     computed: {
         barStyle() {
-            const sf = ScaleUtil.sinify(scale)
+            const sf = this.sf 
             const sf1 = ScaleUtil.divideScale(sf, 0, 2)
             const sf2 = ScaleUtil.divideScale(sf, 1, 2)
             const hSize = h / 8 
             const left = `${w / 2}px`
             const top = `${h / 2 - hSize * sf1}px`
+            const height = `${hSize * (sf1 - sf2)}px`
+            const width = `${w / 20}px`
+            const position = 'absolute'
+            const background = 'indigo'
+            console.log(left, top, position, width, height, background)
+            return {left, top, position, width, height, background}
         }
     }
 })
 
-const app = new Vue({
+const vueInstance = new Vue({
     el : '#app'
 })
