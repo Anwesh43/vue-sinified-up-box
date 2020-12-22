f3499ca556a899710654924780431c95afb4cb81
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 16:48:53 +0530

    adding sinifiedupdom element

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const delay = 20 
 const scGap = 0.02 
+const w = window.innerWidth 
+const h = window.innerHeight
 
 class ScaleUtil {
 
@@ -46,3 +48,32 @@ class State {
         }
     }
 }
+const animator = new Animator()
+
+Vue.component('sinified-up-box', {
+    template: '<div><button  @click="start">start</button><div :style = "barstyle"></div>',
+    data() {
+        return {
+            state: new State()
+        }
+    }, 
+    methods: {
+        start() {
+            animator.start(() => {
+                this.state.update(() => {
+                    animator.stop()
+                })
+            })
+        }
+    },
+    computed: {
+        barStyle() {
+            const sf = ScaleUtil.sinify(scale)
+            const sf1 = ScaleUtil.divideScale(sf, 0, 2)
+            const sf2 = ScaleUtil.divideScale(sf, 1, 2)
+            const hSize = h / 8 
+            const left = `${w / 2}px`
+            const top = `${h / 2 - hSize * sf1}px`
+        }
+    }
+})
